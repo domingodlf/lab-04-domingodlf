480313766c9438485ffae7f9a663590ae73c5fa1
Domingo De la Fuente <ddelafuente@miuandes.cl>
2026-04-16 18:34:15 -0400

    Agrego controllers y rutas base para index y show (Lab 4)

--- vet_clinic/config/routes.rb
+++ vet_clinic/config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  resources :owners, only: [:index, :show]
+  resources :pets, only: [:index, :show]
+  resources :vets, only: [:index, :show]
+  resources :appointments, only: [:index, :show]
+
+  root "owners#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
